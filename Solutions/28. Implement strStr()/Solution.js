33e4347da95cf0808049772ef3a692122b156307
William Crutchfield <william.r.crutchfield@gmail.com>
2019-08-08 22:48:32 -0700

    Solutions (x3)
    Problems 28, 35, 445

---
+++ Solutions/28. Implement strStr()/Solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  if (needle === '') return 0;
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] != needle[0]) continue;
+    else if (haystack.substring(i, i + needle.length) === needle) {
+      return i;
+    }
+  }
+
+  return -1;
+};
